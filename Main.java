202b2b16268ae0b6358ba9bd27f03c5a56c0be2e
PatrikStrausz <55127576+PatrikStrausz@users.noreply.github.com>
2019-09-29 17:34:41 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,102 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        for (int i = 1000; i < 10000; i++) {
+//            if (i % 615 == 0) {                      //1
+//                System.out.print(i + " ");
+//            }
+//            }
+
+//for(int i = 100; i<=999; i++){
+//    int c = i%10;
+//    int b = (i/10)%10;
+//    int a = (i/100)%10;                 //2
+//    if( (a>b) && (a>c) && (c<b)){
+//        System.out.print(i + " ");
+//    }
+//}
+
+
+//        int count = 0;
+//        for(int i = 10; i<100;i++){
+//            count ++;
+//            if(i%3==0){                                //3
+//                count+=i;
+//                System.out.print(count + " ");
+//            }
+//        }
+
+
+//        int count = 0;
+//        for (int i = 1; i <= 1000000; i++) {
+//
+//            if (1000000 % i == 0) {                         //4
+//
+//                count++;
+//                System.out.print(count + " ");
+//
+//            }
+//
+//        }
+
+
+//        int a= 45;
+//        int b= 87;
+//        int c= 37;
+//       int surface = (2*a*b) +(2*a*c) +(2*b*c);         //5
+//       int capacity= a*b*c;
+//        System.out.println("Surface is = " +surface);
+//        System.out.println("Capacity is = " +capacity);
+
+
+        //        for( double i= 10; i>=2; i-=0.5){
+        //            System.out.print(i + " ");            //6
+        //        }
+
+
+//        int fact = 1;
+//        for(int i=7; i>=1; i--){                  //7
+//            fact= fact * i;
+//            System.out.println(fact+ "  " );
+//        }
+
+//        for(int i=100; i<10000; i++){
+//            if (i%2!=0){
+//                if((i%27==0) && (i%13!=0)){          //8
+//                    System.out.print(i + " ");
+//                }
+//
+//            }
+        //      }
+
+
+//        for(int i=91; i<=10000; i+=91){              //9
+//            System.out.print(i + " ");
+//        }
+
+
+
+
+//        for(int i=10000;i<=99999;i++){
+//            int a= i%10;
+//            int b= (i/10)%10;                    //10
+//            int c=(i/1000)%10;
+//            int e=(i/10000)%10;
+//            if((a==e) && (b==c)){
+//                System.out.print(i+ " ");
+//            }
+//        }
+
+
+    }
+}
+
+
+
+
+
+
+
